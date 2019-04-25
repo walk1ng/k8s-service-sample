e2082997c86db9f86d53b7619f49674e661caf45
Wei Li (Wicresoft) <iliwgg@gmail.com>
2019-04-25 11:14:35 +0800

    bug fix of readiness probe

--- handlers/readyz.go
+++ handlers/readyz.go
@@ -8,7 +8,7 @@ import (
 // readyz is a readiness probe
 func readyz(isReady *atomic.Value) http.HandlerFunc {
 	return func(w http.ResponseWriter, _ *http.Request) {
-		if isReady == nil || isReady.Load().(bool) {
+		if isReady == nil || !isReady.Load().(bool) {
 			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
 			return
 		}
